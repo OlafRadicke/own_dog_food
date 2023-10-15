19bacf2387fbb274e46980c522bcd859f5c2cb32
Olaf Radicke <briefkasten@olaf-radicke.de>
2023-10-15 23:19:32 +0200

    WIP

--- pulumi/baltic-sea/go.mod
+++ pulumi/baltic-sea/go.mod
@@ -54,6 +54,7 @@ require (
 	github.com/pjbgf/sha1cd v0.3.0 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pkg/term v1.1.0 // indirect
+	github.com/pulumi/pulumi-kubernetes/sdk/v3 v3.30.2 // indirect
 	github.com/rivo/uniseg v0.4.4 // indirect
 	github.com/rogpeppe/go-internal v1.9.0 // indirect
 	github.com/sabhiram/go-gitignore v0.0.0-20210923224102-525f6e181f06 // indirect
--- pulumi/baltic-sea/main.go
+++ pulumi/baltic-sea/main.go
@@ -1,44 +1,48 @@
 package main
 
+// appsv1 "github.com/pulumi/pulumi-kubernetes/sdk/v4/go/kubernetes/apps/v1"
+// corev1 "github.com/pulumi/pulumi-kubernetes/sdk/v4/go/kubernetes/core/v1"
+// metav1 "github.com/pulumi/pulumi-kubernetes/sdk/v4/go/kubernetes/meta/v1"
+
 import (
-	appsv1 "github.com/pulumi/pulumi-kubernetes/sdk/v4/go/kubernetes/apps/v1"
-	corev1 "github.com/pulumi/pulumi-kubernetes/sdk/v4/go/kubernetes/core/v1"
-	metav1 "github.com/pulumi/pulumi-kubernetes/sdk/v4/go/kubernetes/meta/v1"
+	"github.com/pulumi/pulumi-kubernetes/sdk/v3/go/kubernetes/yaml"
+	"github.com/pulumi/pulumi-kubernetes/sdk/v4/go/kubernetes/helm/v3"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
-func main() {
-	pulumi.Run(func(ctx *pulumi.Context) error {
+var nameOfNamespace = "my-namespace"
 
-		appLabels := pulumi.StringMap{
-			"app": pulumi.String("nginx"),
-		}
-		deployment, err := appsv1.NewDeployment(ctx, "app-dep", &appsv1.DeploymentArgs{
-			Spec: appsv1.DeploymentSpecArgs{
-				Selector: &metav1.LabelSelectorArgs{
-					MatchLabels: appLabels,
-				},
-				Replicas: pulumi.Int(1),
-				Template: &corev1.PodTemplateSpecArgs{
-					Metadata: &metav1.ObjectMetaArgs{
-						Labels: appLabels,
-					},
-					Spec: &corev1.PodSpecArgs{
-						Containers: corev1.ContainerArray{
-							corev1.ContainerArgs{
-								Name:  pulumi.String("nginx"),
-								Image: pulumi.String("nginx"),
-							}},
-					},
+var myNamespace = func(ctx *pulumi.Context) error {
+	_, err := yaml.NewConfigFile(ctx, nameOfNamespace, &yaml.ConfigFileArgs{
+		File: "namespace.yaml",
+	})
+	if err != nil {
+		return err
+	}
+
+	_, err = helm.NewChart(ctx, "nginx-ingress", helm.ChartArgs{
+		Namespace: pulumi.String(nameOfNamespace),
+		Chart:     pulumi.String("nginx-ingress"),
+		Version:   pulumi.String("1.24.4"),
+		FetchArgs: helm.FetchArgs{
+			Repo: pulumi.String("https://charts.helm.sh/stable"),
+		},
+		Values: pulumi.Map{
+			"controller": pulumi.Map{
+				"metrics": pulumi.Map{
+					"enabled": pulumi.Bool(true),
 				},
 			},
-		})
-		if err != nil {
-			return err
-		}
-
-		ctx.Export("name", deployment.Metadata.Name())
-
-		return nil
+		},
 	})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func main() {
+	pulumi.Run(myNamespace)
+	// pulumi.Run(mySetup)
 }
