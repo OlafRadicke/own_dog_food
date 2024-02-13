09b092f8ad809cb47ca9c41a1286a60542c83ff6
Olaf Radicke <briefkasten@olaf-radicke.de>
2024-02-13 11:46:50 +0100

    Deacticate azure setup.

--- pulumi/azure/PulumiGoCode/Montitoring.go
+++ pulumi/azure/PulumiGoCode/Montitoring.go
@@ -3,7 +3,7 @@
 package PulumiGoCode
 
 import (
-	"github.com/pulumi/pulumi-azure-native-sdk/insights/v2"
+	// "github.com/pulumi/pulumi-azure-native-sdk/insights/v2"
 	"github.com/pulumi/pulumi-azure-native-sdk/resources/v2"
 	"github.com/pulumi/pulumi-azure/sdk/v5/go/azure/appinsights"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
@@ -15,6 +15,9 @@ func Montitoring(ctx *pulumi.Context, resourceGroup *resources.ResourceGroup) er
 		Location:          resourceGroup.Location,
 		ResourceGroupName: resourceGroup.Name,
 		ApplicationType:   pulumi.String("web"),
+		Tags: pulumi.StringMap{
+			"deploy": pulumi.String("pulumi"),
+		},
 	})
 	if err != nil {
 		return err
@@ -22,47 +25,59 @@ func Montitoring(ctx *pulumi.Context, resourceGroup *resources.ResourceGroup) er
 	ctx.Export("instrumentationKey", simpleCheck.InstrumentationKey)
 	ctx.Export("appId", simpleCheck.AppId)
 
-	standardWebTest, err2 := appinsights.NewStandardWebTest(ctx, "olaf-radicke-de", &appinsights.StandardWebTestArgs{
+	_, err2 := appinsights.NewStandardWebTest(ctx, "webtest-01", &appinsights.StandardWebTestArgs{
 		ResourceGroupName:     resourceGroup.Name,
-		Location:              pulumi.String("West Europe"),
 		ApplicationInsightsId: simpleCheck.ID(),
 		GeoLocations: pulumi.StringArray{
-			pulumi.String("example"),
+			pulumi.String("emea-nl-ams-azr"),
 		},
 		Request: &appinsights.StandardWebTestRequestArgs{
 			Url: pulumi.String("https://olaf-radicke.de"),
 		},
+		Frequency: pulumi.Int(900),
+		Location:  resourceGroup.Location,
+		Name:      pulumi.String("olaf-radicke-de"),
+		Tags: pulumi.StringMap{
+			"deploy": pulumi.String("pulumi"),
+		},
+		Timeout: pulumi.Int(120),
 	})
 	if err2 != nil {
 		return err
 	}
 
-	_, err = insights.NewAlertRule(ctx, "alertRule", &insights.AlertRuleArgs{
-		Actions: pulumi.Array{},
-		Condition: insights.ThresholdRuleCondition{
-			DataSource: insights.RuleMetricDataSource{
-				MetricName:  "Requests",
-				OdataType:   "Microsoft.Azure.Management.Insights.Models.RuleMetricDataSource",
-				ResourceUri: standardWebTest.Name,
-				// ResourceUri: "/subscriptions/b67f7fec-69fc-4974-9099-a26bd6ffeda3/resourceGroups/Rac46PostSwapRG/providers/Microsoft.Web/sites/leoalerttest",
-			},
-			OdataType:       "Microsoft.Azure.Management.Insights.Models.ThresholdRuleCondition",
-			Operator:        insights.ConditionOperatorGreaterThan,
-			Threshold:       3,
-			TimeAggregation: insights.TimeAggregationOperatorTotal,
-			WindowSize:      "PT5M",
-		},
-		Description:       pulumi.String("alert rule for availability test \"website-the-independent-friend-de-website-monitoring\""),
-		IsEnabled:         pulumi.Bool(true),
-		Location:          pulumi.String("global"),
-		Name:              pulumi.String("website-the-independent-friend-de-website-monitoring"),
-		ResourceGroupName: resourceGroup.Name,
-		RuleName:          pulumi.String("website-the-independent-friend-de-website-monitoring"),
-		Tags:              nil,
-	})
-	if err != nil {
-		return err
-	}
+	// _, err = insights.NewAlertRule(ctx, "alertRule", &insights.AlertRuleArgs{
+	// 	// Action: insights.RuleEmailActionArgs{
+	// 	// 	CustomEmails: {
+	// 	// 		pulumi.String("briefkasten@olaf-radicke.de")
+	// 	// 	},
+	// 	// },
+	// 	Condition: insights.ThresholdRuleCondition{
+	// 		DataSource: insights.RuleMetricDataSource{
+	// 			MetricName:  &pulumi.String("Requests"),
+	// 			OdataType:   pulumi.String("Microsoft.Azure.Management.Insights.Models.RuleMetricDataSource"),
+	// 			ResourceUri: standardWebTest.Name,
+	// 			// ResourceUri: "/subscriptions/b67f7fec-69fc-4974-9099-a26bd6ffeda3/resourceGroups/Rac46PostSwapRG/providers/Microsoft.Web/sites/leoalerttest",
+	// 		},
+	// 		OdataType:       pulumi.String("Microsoft.Azure.Management.Insights.Models.ThresholdRuleCondition"),
+	// 		Operator:        insights.ConditionOperatorGreaterThan,
+	// 		Threshold:       3,
+	// 		TimeAggregation: insights.TimeAggregationOperatorTotal,
+	// 		WindowSize:      pulumi.String("PT5M"),
+	// 	},
+	// 	Description:       pulumi.String("alert rule for availability test \"olaf-radicke-de\""),
+	// 	IsEnabled:         pulumi.Bool(true),
+	// 	Location:          pulumi.String("global"),
+	// 	Name:              pulumi.String("olaf-radicke-de"),
+	// 	ResourceGroupName: resourceGroup.Name,
+	// 	RuleName:          pulumi.String("olaf-radicke-de"),
+	// 	Tags: pulumi.StringMap{
+	// 		"deploy": pulumi.String("pulumi"),
+	// 	},
+	// })
+	// if err != nil {
+	// 	return err
+	// }
 
 	return nil
 }
--- pulumi/azure/run.sh
+++ pulumi/azure/run.sh
@@ -5,12 +5,26 @@ set -e
 set -u
 
 export PULUMI_CONFIG_PASSPHRASE_FILE=${HOME}/.ssh/pulumi-passwd
-START_PWD=$(pwd)
+export START_PWD=$(pwd)
+export WORK_DIR="pulumi/azure"
 
-cd pulumi/azure
+if [ -z ${INSTALL_AND_UPDATE_GO_MODS+x} ]
+then
+	INSTALL_AND_UPDATE_GO_MODS="FALSE"
+	echo "var is set to '$INSTALL_AND_UPDATE_GO_MODS'"
+else
+	echo "var is set to '$INSTALL_AND_UPDATE_GO_MODS'"
+	# INSTALL_AND_UPDATE_GO_MODS="TRUE"
+fi
+
+cd $WORK_DIR
 go mod tidy
-# go get -u
-
+if [ ${INSTALL_AND_UPDATE_GO_MODS} == "TRUE" ]
+then
+	go get -u -t -x
+else
+	echo "don't update go modules."
+fi
 
 gcloud config set project pulumi-prod
 pulumi login gs://pulumi-azure
