c2e38eb064ff049f5853fd6a1f38af44f8764536
Olaf Radicke <briefkasten@olaf-radicke.de>
2025-04-23 17:17:00 +0200

    Add vault manifest resources.

---
+++ scrips/port-forward/vault.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -x
+set -e
+set -u
+
+MY_NAMESPACE=vault-server
+MY_SERVICE=vault
+MY_PORT_IN=8200
+MY_PORT_OUT=8082
+
+echo "go to: http://localhost:${MY_PORT_OUT}/ui"
+# kubectx atlantic-ocean
+kubectl -n ${MY_NAMESPACE} port-forward service/${MY_SERVICE} ${MY_PORT_OUT}:${MY_PORT_IN}
+# firefox http://localhost:${MY_PORT}/ &&
