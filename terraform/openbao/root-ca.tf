7c1f115d7d918f5535756cc4ae5ec4849c69f437
Olaf Radicke <briefkasten@olaf-radicke.de>
2025-05-13 11:20:29 +0200

    Feat(#10): Fix pki config.

--- terraform/openbao/root-ca.tf
+++ terraform/openbao/root-ca.tf
@@ -36,8 +36,46 @@ resource "vault_mount" "pki-issuer" {
 resource "vault_mount" "pki" {
   path = "pki"
   type = "pki"
+  description = "PKI Secrets Engine"
 }
 
+# Workarount https://www.infralovers.com/de/blog/2023-10-16-hashicorp-vault-acme-terraform-configuration/
+resource "vault_generic_endpoint" "root_config_cluster" {
+  depends_on           = [vault_mount.pki]
+  path                 = "${vault_mount.pki.path}/config/cluster"
+  ignore_absent_fields = true
+  disable_delete       = true
+
+  data_json = <<EOT
+{
+    "aia_path": "http://openbao.openbao:8200/v1/${vault_mount.pki.path}",
+    "path": "http://openbao.openbao:8200/v1/${vault_mount.pki.path}"
+}
+EOT
+}
+
+resource "vault_generic_endpoint" "root_config_urls" {
+  depends_on           = [vault_mount.pki, vault_generic_endpoint.root_config_cluster]
+  path                 = "${vault_mount.pki.path}/config/urls"
+  ignore_absent_fields = true
+  disable_delete       = true
+
+  data_json = <<EOT
+{
+    "enable_templating": true,
+    "issuing_certificates": "{{cluster_aia_path}}/issuer/{{issuer_id}}/der",
+    "crl_distribution_points": "{{cluster_aia_path}}/issuer/{{issuer_id}}/crl/der",
+    "ocsp_servers": "{{cluster_path}}/ocsp"
+}
+EOT
+}
+
+resource "vault_pki_secret_backend_role" "server-pki" {
+  backend        = vault_mount.pki.path
+  name           = "server-pki"
+  no_store       = false
+  allow_any_name = true
+}
 
 # https://registry.terraform.io/providers/hashicorp/vault/latest/docs/resources/pki_secret_backend_root_cert#backend-1
 
@@ -45,7 +83,7 @@ resource "vault_pki_secret_backend_root_cert" "root_ca" {
   depends_on            = [vault_mount.pki]
   backend               = "http://openbao.openbao:8200"
   type                  = "internal"
-  common_name           = "Root CA"
+  common_name           = "root-ca.irish.sea"
   ttl                   = "315360000"
   format                = "pem"
   private_key_format    = "der"
@@ -55,3 +93,18 @@ resource "vault_pki_secret_backend_root_cert" "root_ca" {
   ou                    = "irish sea"
   organization          = "own dog food"
 }
+
+
+# resource "vault_pki_secret_backend_role" "example_role" {
+#   backend         = vault_mount.pki.path
+#   name            = "example-role"
+#   allowed_domains = ["example.com"]
+#   allow_subdomains = true
+#   max_ttl         = "720h"
+# }
+
+# resource "vault_pki_secret_backend_cert" "example_cert" {
+#   backend     = vault_mount.pki.path
+#   name        = vault_pki_secret_backend_role.example_role.name
+#   common_name = "app.example.com"
+# }
