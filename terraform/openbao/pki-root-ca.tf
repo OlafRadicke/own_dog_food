842cc65d89bfcdc2d9a93c69746cdb7218d87c19
Olaf Radicke <briefkasten@olaf-radicke.de>
2025-05-22 15:35:14 +0200

    Feat(#10): fix first sub ca.

--- terraform/openbao/pki-root-ca.tf
+++ terraform/openbao/pki-root-ca.tf
@@ -57,24 +57,24 @@ resource "vault_pki_secret_backend_config_ca" "root_ca_config" {
 }
 
 
-# resource "vault_pki_secret_backend_root_cert" "root_ca" {
-#   depends_on            = [vault_mount.pki_root_ca]
-#   backend               = vault_mount.pki_root_ca.path
-#   type                  = "internal"
-#   common_name           = "Irish sea root CA 01"
-#   # ttl                   = "315360000"
-#   ttl          = "175200h" # 20 Jahre
-#   format                = "pem"
-#   private_key_format    = "der"
-#   key_type              = "rsa"
-#   key_bits              = 4096
-#   exclude_cn_from_sans  = true
-#   ou                    = "irish sea"
-#   organization          = "own dog food"
-#   country               = "DE"
-#   locality              = "Krefeld"
-#   province              = "NRW"
-# }
+resource "vault_pki_secret_backend_root_cert" "root_ca" {
+  depends_on  = [vault_mount.pki_root_ca]
+  backend     = vault_mount.pki_root_ca.path
+  type        = "internal"
+  common_name = "Irish sea root CA 01"
+  # ttl                   = "315360000"
+  ttl                  = "175200h" # 20 Jahre
+  format               = "pem"
+  private_key_format   = "der"
+  key_type             = "rsa"
+  key_bits             = 4096
+  exclude_cn_from_sans = true
+  ou                   = "irish sea"
+  organization         = "own dog food"
+  country              = "DE"
+  locality             = "Krefeld"
+  province             = "NRW"
+}
 
 
 # resource "vault_pki_secret_backend_issuer" "issuer_root_ca" {
