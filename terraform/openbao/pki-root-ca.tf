564ffbbaa7bca501c653d27deb39ff3c3516ba3d
Olaf Radicke <briefkasten@olaf-radicke.de>
2025-05-22 15:59:07 +0200

    Feat(#10): fix first sub ca.

--- terraform/openbao/pki-root-ca.tf
+++ terraform/openbao/pki-root-ca.tf
@@ -6,36 +6,36 @@ provider "vault" {
 }
 
 
-resource "tls_private_key" "root-ca-key" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
-}
+# resource "tls_private_key" "root-ca-key" {
+#   algorithm = "RSA"
+#   rsa_bits  = 4096
+# }
 
 
-resource "tls_self_signed_cert" "root_ca_cert" {
-  depends_on = [
-    tls_private_key.root-ca-key,
-  ]
-  private_key_pem = tls_private_key.root-ca-key.private_key_pem
-  # key_algorithm = "RSA"
-  subject {
-    common_name         = "Irish sea root CA 01"
-    organization        = "own dog food"
-    organizational_unit = "irish sea"
-    country             = "DE"
-    locality            = "Krefeld"
-    province            = "NRW"
+# resource "tls_self_signed_cert" "root_ca_cert" {
+#   depends_on = [
+#     tls_private_key.root-ca-key,
+#   ]
+#   private_key_pem = tls_private_key.root-ca-key.private_key_pem
+#   # key_algorithm = "RSA"
+#   subject {
+#     common_name         = "Irish sea root CA 01"
+#     organization        = "own dog food"
+#     organizational_unit = "irish sea"
+#     country             = "DE"
+#     locality            = "Krefeld"
+#     province            = "NRW"
 
-  }
-  # 20 Jahre
-  validity_period_hours = 175200
-  allowed_uses = [
-    "cert_signing",
-    "crl_signing",
-    "server_auth",
-  ]
-  is_ca_certificate = true
-}
+#   }
+#   # 20 Jahre
+#   validity_period_hours = 175200
+#   allowed_uses = [
+#     "cert_signing",
+#     "crl_signing",
+#     "server_auth",
+#   ]
+#   is_ca_certificate = true
+# }
 
 
 
