97d3cb0d5ad141a905b2d0852953ef60baf2e46e
Olaf Radicke <briefkasten@olaf-radicke.de>
2025-06-08 13:31:11 +0200

    Feat(#10): Change  service-01 CSR.

--- terraform/openbao/pki-leaf.tf
+++ terraform/openbao/pki-leaf.tf
@@ -37,24 +37,22 @@ resource "vault_mount" "service_01" {
 
 # Alternativ: vault_pki_secret_backend_sign
 
-# resource "vault_pki_secret_backend_cert_request" "service-01" {
-#   backend            = vault_mount.service_01.path
-#   name               = "service-01"
-#   common_name        = "service-01.irish.sea"
-#   alt_names          = ["www.service-01.irish.sea"]
-#   ttl                = "8760h"
-#   format             = "pem"
-#   private_key_format = "pkcs8"
-# }
+resource "vault_pki_secret_backend_intermediate_cert_request" "service-01" {
+  backend            = vault_mount.service_01.path
+  common_name        = "service-01.irish.sea"
+  format             = "pem"
+  private_key_format = "pkcs8"
+  type               = "internal"
+}
 
-# resource "vault_pki_secret_backend_sign" "service-01" {
-#   backend     = vault_mount.policy_ca_01.path
-#   name        = "service-01"
-#   common_name = "service-01.irish.sea"
-#   csr         = vault_pki_secret_backend_cert_request.service-01.csr
-#   format      = "pem_bundle"
-#   ttl         = "8760h"
-# }
+resource "vault_pki_secret_backend_sign" "service-01" {
+  backend     = vault_mount.policy_ca_01.path
+  name        = "service-01"
+  common_name = "service-01.irish.sea"
+  csr         = vault_pki_secret_backend_cert_request.service-01.csr
+  format      = "pem_bundle"
+  ttl         = "8760h"
+}
 
 # resource "vault_pki_secret_backend_cert" "service-02" {
 #   backend     = vault_mount.policy_ca_01.path
@@ -64,30 +62,30 @@ resource "vault_mount" "service_01" {
 #   ttl         = "8760h"
 # }
 
-resource "vault_pki_secret_backend_role" "server-01" {
-  #   backend            = vault_mount.service_01.path
-  #   backend            = vault_mount.policy_ca_01.path
-  backend            = "policy_ca_01"
-  name               = "Service 01"
-  allowed_domains    = ["service-01.irish.sea"]
-  allow_subdomains   = true
-  allow_glob_domains = false
-  allow_any_name     = false
-  enforce_hostnames  = true
-  allow_ip_sans      = true
-  server_flag        = true
-  client_flag        = false
-  ou                 = ["irish sea"]
-  organization       = ["own dog food"]
-  country            = ["DE"]
-  locality           = ["Krefeld"]
-  province           = ["NRW"]
-  ttl                = vault_mount.service_01.default_lease_ttl_seconds
-  no_store           = true
-}
+# resource "vault_pki_secret_backend_role" "server-01" {
+#   #   backend            = vault_mount.service_01.path
+#   #   backend            = vault_mount.policy_ca_01.path
+#   backend            = "policy_ca_01"
+#   name               = "Service 01"
+#   allowed_domains    = ["service-01.irish.sea"]
+#   allow_subdomains   = true
+#   allow_glob_domains = false
+#   allow_any_name     = false
+#   enforce_hostnames  = true
+#   allow_ip_sans      = true
+#   server_flag        = true
+#   client_flag        = false
+#   ou                 = ["irish sea"]
+#   organization       = ["own dog food"]
+#   country            = ["DE"]
+#   locality           = ["Krefeld"]
+#   province           = ["NRW"]
+#   ttl                = vault_mount.service_01.default_lease_ttl_seconds
+#   no_store           = true
+# }
 
-resource "vault_pki_secret_backend_cert" "app" {
-  backend     = vault_mount.policy_ca_01.path
-  name        = vault_pki_secret_backend_role.server-01.name
-  common_name = "service-01.irish.sea"
-}
+# resource "vault_pki_secret_backend_cert" "app" {
+#   backend     = vault_mount.policy_ca_01.path
+#   name        = vault_pki_secret_backend_role.server-01.name
+#   common_name = "service-01.irish.sea"
+# }
