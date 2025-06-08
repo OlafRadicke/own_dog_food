ab45c8bd1ab6dd0b4b9a712257b3b3d7f409da40
Olaf Radicke <briefkasten@olaf-radicke.de>
2025-06-08 11:28:17 +0200

    Feat(#10): Add leaf first config.

--- terraform/openbao/pki-leaf.tf
+++ terraform/openbao/pki-leaf.tf
@@ -10,38 +10,37 @@ resource "vault_mount" "pki_service_01" {
   max_lease_ttl_seconds     = "2592000" # 30 days
 }
 
-# #
+
 # # Role for server certs
 # # This creates certs of machinename.mydomain.com
-# resource "vault_pki_secret_backend_role" "role-server-cer-01" {
-#   depends_on = [
-#     vault_mount.pki_service_01,
-#     vault_pki_secret_backend_root_sign_intermediate.policy_ca_01
-#   ]
-#   backend = vault_mount.pki_service_01.path
-#   name    = "Service 01"
-#   allowed_domains = [
-#     "my.fun",
-#     "your.fun",
-#   ]
-#   allow_subdomains   = true
-#   allow_glob_domains = false
-#   allow_any_name     = false
-#   enforce_hostnames  = true
-#   allow_ip_sans      = true
-#   server_flag        = true
-#   client_flag        = false
-#   ou                 = ["irish sea"]
-#   organization       = ["own dog food"]
-#   country            = ["DE"]
-#   locality           = ["Krefeld"]
-#   province           = ["NRW"]
-#   # 2 years
-#   max_ttl = 63113904
-#   # 30 days
-#   ttl      = 2592000
-#   no_store = true
+resource "vault_pki_secret_backend_role" "role-server-cer-01" {
+  depends_on = [
+    vault_mount.pki_service_01,
+    vault_pki_secret_backend_root_sign_intermediate.policy_ca_01
+  ]
+  backend = vault_mount.pki_service_01.path
+  name    = "Service 01"
+  allowed_domains = [
+    "my.fun",
+    "your.fun",
+  ]
+  allow_subdomains   = true
+  allow_glob_domains = false
+  allow_any_name     = false
+  enforce_hostnames  = true
+  allow_ip_sans      = true
+  server_flag        = true
+  client_flag        = false
+  common_name        = "Service 01"
+  ou                 = ["irish sea"]
+  organization       = ["own dog food"]
+  country            = ["DE"]
+  locality           = ["Krefeld"]
+  province           = ["NRW"]
+  max_ttl            = vault_mount.pki_service_01.default_lease_ttl_seconds
+  ttl                = vault_mount.pki_service_01.default_lease_ttl_seconds
+  no_store           = true
 
-# }
+}
 
 # Alternativ: vault_pki_secret_backend_sign
